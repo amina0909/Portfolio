31c9af28e70d7f0de2ab7367b818faa0be4302b3
amina0909 <57429018+amina0909@users.noreply.github.com>
2020-04-20 23:48:04 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+ <html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Amina Edavalath, Seventh Period]</p>'; ?> 
+    <ul class= "nav nav-tabs">
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Last_AboutMe.html">Amina_AboutMe.html</a></li></br>
+      <li><a href="Last_Projects.html">Amina_Projects.html</a></li></br>
+      <li><a href="Last_Resume.html">Amina_Resume.html</a></li></br>
+      <li><a href="#">Amina_ContactMe.html</a></li></br>
+    </ul>
+  </body>
+  <footer>
+<div>
+<img src="images/DHS2.png">
+</div>
+  </footer>
+</html>
